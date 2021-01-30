45c0f81e4baaf36e5180f54105b1a1ab24f323c0
rabbit-isdead <madeline.hops@gmail.com>
2021-01-30 15:47:51 -0500

    activity2: initial setup

---
+++ activites/slamina/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
