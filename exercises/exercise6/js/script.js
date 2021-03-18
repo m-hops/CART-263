614403b88ca944c8fb0c2db759c89764ea03450d
rabbit-isdead <madeline.hops@gmail.com>
2021-03-18 14:41:05 -0400

    EXERCISE 6: FEATURES ADDED - ADDED COUNTDOWN THAT WHEN ALL REDACTED IS REVEALED, PAGE WILL SWITCH - ADDED LISTENER THAT WILL CHANGE PAGE BACK UPON VIDEO COMPLETION

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -8,8 +8,10 @@ MADELINE ZAYTSOFF
 IMPROVEMENTS MADE TO ORIGINAL ACTIVITY:
 
 1. STYLIZED WITH WITH CSS AND HTML
-2.
-3.
+2. TEXT FLICKERING VIA CSS ANIMATION
+3. MONITOR THE AMOUNT OF .REDACTED CLASS TO SWITCH OVER TO ANOTHER PAGE WHEN ALL IS REVEALED
+4. UPDATED TEXT CONTENT
+5. ADDED LISTENER TO LOOP BACK TO BEGINNING AFTER VIDEO FINISHES PLAYING
 
 ******************/
 
@@ -29,9 +31,22 @@ function revelation() {
 
 function attemptReveal() {
   let r = Math.random();
+  let n = $('.redacted').length;
 
   if (r <0.1) {
+    console.log(n);
+
     $(this).removeClass('redacted');
+
+    if (n <= 1){
+      window.location.href = 'playVid.html'
+    }
+
     $(this).addClass('revealed')
   }
+}
+
+var video = document.getElementById('simpsons');
+video.onended = function() {
+  window.location.href = 'index.html';
 }
