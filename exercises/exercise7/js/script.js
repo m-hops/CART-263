4be089805c7666c9512ab673aca333af3d6ea8f2
rabbit-isdead <madeline.hops@gmail.com>
2021-03-23 12:34:31 -0400

    EXERCISE 7: INITAL UPLOAD

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,37 @@
+/**
+ACTIVITY 8: CODE-TAKER
+MADELINE ZAYTSOFF
+
+*/
+
+"use strict";
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  })
+})
+
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+
+    }
+  }
+});
